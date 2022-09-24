00cd16b9bc92a5f37a7c8c0517a6083a1dbe5ec3
daniel21connor <jconnorr1@miumg.edu.gt>
2022-09-24 10:38:39 -1100

    se agregaron los metodos para la conexcion en la base de datos

--- src/java/clases/TiendaParcial.java
+++ src/java/clases/TiendaParcial.java
@@ -1,13 +1,32 @@
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package clases;
+package Clases;
 
-/**
- *
- * @author PC GAMING
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TiendaParcial {
+    
+    private String url="jdbc:mysql://localhost:3306/tienda";//url de MySQL
+    private String usuario="root";// usuario de mysql local
+    private String clave="Daniel2121"; 
+    private Connection conexion=null;  
+    
+    public Connection conectar(){   
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion=DriverManager.getConnection( url, usuario,clave);
+        }  catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {   
+           ex.printStackTrace();
+        }         
+        return conexion;        
+    }
     
 }
